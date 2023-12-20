74d6f9116c1c2c1644b1a97095e9a95ea507c62e
WyShadow <luujason11@gmail.com>
2023-12-19 22:28:30 -0600

    Created navBar

---
+++ components/Navbar.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return(
+    <div className={styles.container}>
+      Hello
+    </div>
+  )
+}
+
+
+export default Navbar;
